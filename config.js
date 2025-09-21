2b0042cf84988928ceaf0edee21444847bac0b3d
Vastavikrahul01 <165668531+Vastavikrahul01@users.noreply.github.com>
2025-09-21 19:22:15 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 // config.js - Edit these values
 // DEFAULT_M3U8: default stream loaded on page (optional)
-window.DEFAULT_M3U8 = "";
+window.DEFAULT_M3U8 = "https://dish.slivcdn.com/hls/live/2020591/TEN3HD/master_664.m3u8";
 
 // JOIN_LINKS: up to 4 buttons (title and url)
 window.JOIN_LINKS = [
